debbb13d2b498a5ddc40b772e1fb03fb9523728a
Rowan Barnard <rowan@kineticskunk.io>
2023-05-21 14:30:01 +0200

    first push

---
+++ Readme.md
@@ -0,0 +1,68 @@
+# Alpine releases
+
+Download link: `https://www.alpinelinux.org/downloads/`
+
+# Install alpine
+Localhost login: `root`
+Run `setup-alpine`
+
+## Example of Alpine setup
+
+Select keyboard layout [none]: `us`
+Select variant (or 'aboort'): `us`
+
+Enter system hostname [localhost]: Press Enter
+
+Available interfaces are: eth0 eth1
+Which one do you want to initialize? [eth0]: Press Enter
+Ip address for eth0? [dhcp]: Press Enter
+Do you want any manual network configuration? (y/n) [n]: Press Enter
+
+New Password: `root`
+Retype Password: `root`
+
+Which Timezone are you in? ('?' for list) [UTC]: `Europe/Berlin`
+
+HTTP/FTP proxy URL? [none]: Press Enter
+Which NTP client to run? ('busybox','openntpd','chrony' or 'none') [chrony]: `busybox`
+
+press `q`
+-### Note: r = random
+Enter mirror number (1-72) or URL to add (or r/f/e/done) [1]: `r`
+
+Setup a user? (enter a lower-case loginname, or 'no') [no]: Press Enter
+Which server? ('openssh','dropbear', or none) [openssh]: Press Enter
+Allow root ssh login? ('?' for help) [phrohibit-password]: Press Enter
+Enter ssh key or URL for root (or 'none') [none]: Press Enter
+
+Available disks are: sda
+Which disks would you like to use? [none]: `sda`
+How would you like to use it? ('sys', 'data', 'crypt', 'lvm' or '?') [?]: `sys`
+WARNING: Erase above disk(s) and continue? (y/n) [n]: `y`
+
+## Mount shared folder
+"vbox_shared" is the name of the shared folder
+`mkdir -p /mnt/shared; modprobe -a vboxsf; mount -t vboxsf vbox_shared /mnt/shared`
+
+## Install Kubernetes
+Run the folloing command
+/mnt/shared/init.sh
+
+## Register Master Node
+Update hostname
+`hostname master-1`
+`echo "master-1" > /etc/hostname`
+Get exact kubernetes version
+`kubeadm version`
+Get IP address
+`ifconfig` 
+Initialise Kubernetes master node, bypass preflight checks for small virtual machines
+`kubeadm init --apiserver-advertise-address=<Master Node IP Here> --kubernetes version=1.26.1 --ignore-preflight-errors=all | tee /mnt/shared/logs`
+You should now be able to use the custom join command in the other VM
+
+## Register Worker Node
+Update hostname
+`hostname worker-1`
+`echo "worker-1" > /etc/hostname`   
+Copy over the uniquely generated command from master logs into 'join.sh', example of the command below:
+`kubeadm join 10.0.0.150:6443 --token xunjoc.yx2m65r8inhxph9i --discovery-token-ca-cert-hash sha256:e38dd277fe1143771dfe17261d9862e5313d1cdf3922ea86f8f73b6c0a515798`
---
+++ init.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+KUBE_VERSION='1.26'
+
+# Update APK library to the latest-stable version
+cat <<'EOT' >> /etc/apk/repositories
+http://dl-cdn.alpinelinux.org/alpine/latest-stable/main
+http://dl-cdn.alpinelinux.org/alpine/latest-stable/community
+http://dl-cdn.alpinelinux.org/alpine/edge/community
+http://dl-cdn.alpinelinux.org/alpine/edge/testing
+EOT
+
+apk update && apk upgrade
+apk update --available
+apk upgrade --available
+
+# Install util packages
+apk add uuidgen \
+        nfs-utils \
+        cni-plugins \
+        cni-plugin-flannel \
+        flannel-contrib-cni \
+        flannel
+
+# Install Kubernetes packages
+apk add kubelet \
+        kubeadm \
+        kubectl \
+        containerd
+
+# Add kernel module for networking
+echo "br_netfilter" > /etc/modules-load.d/k8s.conf
+modprobe br_netfilter
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# Remove swap storage
+cat /etc/fstab | grep -v swap > temp.fstab
+cat temp.fstab > /etc/fstab
+rm temp.fstab
+swapoff -a
+
+# Fix id error messages
+uuidgen > /etc/machine-id
+
+# Add services
+rc-update add containerd
+rc-update add kubelet
+rc-update add ntpd
+
+# Start services
+/etc/init.d/ntpd start
+/etc/init.d/containerd start
+
+# Create flannel Symlink
+ln -s /usr/libexec/cni/flannel-amd64 /usr/libexec/cni/flannel
+
+# Ensure that brigeded packets traverse iptable rules
+echo "net.bridge.bridge-nf-call-iptables=1" >> /etc/sysctl.conf
+sysctl net.bridge.bridge-nf-call-iptables=1
+
+# Pin your versions!  If you update and the nodes get out of sync, it implodes.
+# NOTE: In the future you will manually have to add a newer version the same way to upgrade.
+# ------------------------------------------------------------------------------------------
+# apk add 'kubelet=~1.26'
+# apk add 'kubeadm=~1.26'
+# apk add 'kubectl=~1.26'
+
+apk add kubelet=~${KUBE_VERSION}
+apk add kubeadm=~${KUBE_VERSION}
+apk add kubectl=~${KUBE_VERSION}
+
+# Setup default hostname
+hostname 'worker'
+echo 'worker' > /etc/hostname
+
+# Setup default kubeconfig
+mkdir ~/.kube > /dev/null 2>&1
+ln -s /etc/kubernetes/kubelet.conf /root/.kube/config > /dev/null 2>&1
+
+# Pre-pulling kubernetes images
+kubeadm config images pull
+
+# Reboot countdown to 
+replace="\033[1A\033[K"
+reboot_countdown=5
+
+for (( i=${reboot_countdown}; i >= 1; --i ))
+do 
+    echo "Updating Kernel. Rebooting in $i."
+    sleep 0.5
+    echo -e "${replace}Updating Kernel. Rebooting in $i.."
+    sleep 0.5
+done
+sync
+reboot
